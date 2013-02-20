82e627b329576ac90c6f8f42ba41d92b1e3ca6d0
Sebastian <sebastian@yo.lk>
2013-02-20 02:26:14 +0100

    Fixed warning in Biggs::Format

--- lib/biggs/format.rb
+++ lib/biggs/format.rb
@@ -1,22 +1,20 @@
 module Biggs
   class Format
     attr_reader :country_name, :iso_code, :format_string
-    
-    DEFAULT_WITH_STATE = 
-    
+
     def initialize(iso_code)
       @iso_code = iso_code.to_s.downcase
       @country_name = Biggs.country_names[@iso_code]
       @format_string = Biggs.formats[@iso_code]
     end
-    
+
     class << self
       def find(iso_code)
         entries_cache[iso_code] ||= new(iso_code)
       end
-      
+
       private
-      
+
       def entries_cache
         @entries_cache ||= {}
       end
